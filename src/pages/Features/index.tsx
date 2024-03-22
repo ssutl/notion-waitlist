6d1ba4a2915f167381c94730ab8a9db2a17c512d
shayCamp <76885270+ssutl@users.noreply.github.com>
2024-03-22 03:10:52 +0000

    Reversing deatures

--- src/pages/Features/index.tsx
+++ src/pages/Features/index.tsx
@@ -68,61 +68,65 @@ export default function Features({
       </Head>
       <div className="w-full py-14 px-10 lg:px-12">
         {FeaturesContent.length ? (
-          FeaturesContent.map((feature, index) => {
-            return (
-              <div
-                key={index}
-                className="mb-14 lg:flex lg:flex-row lg:justify-between"
-              >
-                <div className="h-72 w-full relative rounded-md overflow-hidden mb-10 md:mb-8 md:h-96 lg:w-2/5">
-                  <Image
-                    src={
-                      feature.cover.file
-                        ? feature.cover.file.url
-                        : feature.cover.external.url
-                    }
-                    alt="Picture of the author"
-                    layout="fill"
-                    objectFit="cover"
-                  />
-                </div>
-                <span className="lg:w-11/20">
-                  <h1 className="text-4xl font-semibold mb-5 md:mb-6 md:text-5xl">
-                    {feature.properties.Name.title.length > 0
-                      ? feature.properties.Name.title[0].plain_text
-                      : "Title"}
-                  </h1>
-                  <p className="text-base mb-5 md:text-xl">
-                    {feature.properties.Description.rich_text.length > 0
-                      ? feature.properties.Description.rich_text[0].plain_text
-                      : "Description"}
-                  </p>
-                  <p className="text-base mb-5 md:text-xl">
-                    {feature.properties.Date.date !== null &&
-                      new Date(
-                        feature.properties.Date.date.start
-                      ).toLocaleDateString("en-US", {
-                        weekday: "long",
-                        day: "numeric",
-                        month: "long",
-                      })}
-                  </p>
-                  <div className="w-full flex">
-                    {feature.properties.Tags.multi_select.map((tag, index) => {
-                      return (
-                        <p
-                          key={index}
-                          className="w-fit bg-gray-200 rounded-md px-2 py-1 mr-4 text-base md:text-xl"
-                        >
-                          {tag.name}
-                        </p>
-                      );
-                    })}
+          FeaturesContent.slice()
+            .reverse()
+            .map((feature, index) => {
+              return (
+                <div
+                  key={index}
+                  className="mb-14 lg:flex lg:flex-row lg:justify-between"
+                >
+                  <div className="h-72 w-full relative rounded-md overflow-hidden mb-10 md:mb-8 md:h-96 lg:w-2/5">
+                    <Image
+                      src={
+                        feature.cover.file
+                          ? feature.cover.file.url
+                          : feature.cover.external.url
+                      }
+                      alt="Picture of the author"
+                      layout="fill"
+                      objectFit="cover"
+                    />
                   </div>
-                </span>
-              </div>
-            );
-          })
+                  <span className="lg:w-11/20">
+                    <h1 className="text-4xl font-semibold mb-5 md:mb-6 md:text-5xl">
+                      {feature.properties.Name.title.length > 0
+                        ? feature.properties.Name.title[0].plain_text
+                        : "Title"}
+                    </h1>
+                    <p className="text-base mb-5 md:text-xl">
+                      {feature.properties.Description.rich_text.length > 0
+                        ? feature.properties.Description.rich_text[0].plain_text
+                        : "Description"}
+                    </p>
+                    <p className="text-base mb-5 md:text-xl">
+                      {feature.properties.Date.date !== null &&
+                        new Date(
+                          feature.properties.Date.date.start
+                        ).toLocaleDateString("en-US", {
+                          weekday: "long",
+                          day: "numeric",
+                          month: "long",
+                        })}
+                    </p>
+                    <div className="w-full flex">
+                      {feature.properties.Tags.multi_select.map(
+                        (tag, index) => {
+                          return (
+                            <p
+                              key={index}
+                              className="w-fit bg-gray-200 rounded-md px-2 py-1 mr-4 text-base md:text-xl"
+                            >
+                              {tag.name}
+                            </p>
+                          );
+                        }
+                      )}
+                    </div>
+                  </span>
+                </div>
+              );
+            })
         ) : (
           <>
             <h1 className="text-4xl font-semibold mb-10 md:text-5xl md:mb-8">
