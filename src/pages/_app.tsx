cfdf26c4fbad1cec3fea55121ce74fd9d674d1a5
Shay Campbell <shayc.campbell@gmail.com>
2024-03-27 21:32:28 +0000

    Getting analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/Components/Layout";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
     </Layout>
   );
 }
