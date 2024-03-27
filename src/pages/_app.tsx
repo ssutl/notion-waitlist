6a535741ef82f6a7c0ec64bdb109e916e6724c8b
Shay Campbell <shayc.campbell@gmail.com>
2024-03-27 21:29:20 +0000

    Getting analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/Components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
